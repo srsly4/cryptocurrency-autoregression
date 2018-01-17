e2fddbca9581782b901da71d589e5c1aa9c76a2a
elrohil <wieslaw.stanek@spinney.io>
2018-01-17 23:32:37 +0100

    Added messages handling

--- backend/API.py
+++ backend/API.py
@@ -3,20 +3,33 @@ from requests import request
 API_URL = 'https://min-api.cryptocompare.com/'
 
 
-def fetch_history(limit=50):
+def fetch_history(limit=None, period=None, currency=None,
+                  output_currency=None):
+    if not limit:
+        limit = 50
+    if not currency:
+        currency = 'BTC'
+    if not output_currency:
+        output_currency = 'USD'
+    if not period in ['minute', 'day', 'hour']:
+        period = 'minute'
     params = {
-        'fsym': 'BTC',
-        'tsym': 'USD',
+        'fsym': currency,
+        'tsym': output_currency,
         'limit': limit,
     }
-    return request('GET', API_URL + 'data/histominute',
+    return request('GET', API_URL + 'data/histo{}'.format(period),
                    params=params)
 
 
-def get_current():
+def get_current(currency=None, output_currency=None):
+    if not currency:
+        currency = 'BTC'
+    if not output_currency:
+        output_currency = 'USD'
     params = {
-        'fsym': 'BTC',
-        'tsyms': 'USD',
+        'fsym': currency,
+        'tsyms': output_currency,
     }
     return request('GET', API_URL + 'data/price',
                    params=params)
--- backend/server.py
+++ backend/server.py
@@ -1,3 +1,4 @@
+import json
 from json import JSONEncoder
 from threading import Event
 
@@ -6,36 +7,88 @@ import logging
 from websocket_server import WebsocketServer
 from backend.API import fetch_history, get_current
 from backend.cron_updating import Timer
+from random import Random
 
 PORT = 9001
 IP = '127.0.0.1'
 
 
+def random_generator(start, end, period):
+    rand = Random()
+    if start == end:
+        return {'x': [], 'y': []}
+    if start > end:
+        start, end = end, start
+    x = list(range(start, end, period))
+    return {'x': x, 'y': [11500 + 1000 * (rand.random() - 0.5) for _ in x]}
+
+
 def new_client(client, server):
     print('New client connected')
+    print(client)
 
-    res = fetch_history(2000)
-    data = [] if not res else res.json().get('Data')
+
+def data_request(client, server, message):
+    currency, output_currency = \
+        client['currency'], client['output_currency'] = \
+        message.get('currency'), message.get('outputCurrency')
+    res = fetch_history(message.get('records'), currency=currency,
+                        output_currency=output_currency)
+    if res:
+        data = res.json().get('Data', [])
+        msg = {
+            'type': 'INITIAL_STATE',
+            'data': data,
+        }
+        server.send_message(client, JSONEncoder().encode(msg))
+    else:
+        print('Error while fetching data')
+        server.send_message(client, JSONEncoder().encode({'type': 'ERROR', 'msg': 'Couldn\'t fetch data'}))
+
+
+def forecast_request(client, server, message):
+    start = message.get('startTimestamp', 0)
+    end = message.get('endTimestamp', 0)
+    data = random_generator(start, end, 5)
     msg = {
-       'type': 'INITIAL_STATE',
-       'data': data,
+        'type': 'FORECAST',
+        'data': data
     }
-    print(res.json().get('TimeTo'))
-    print(int(time.time()))
     server.send_message(client, JSONEncoder().encode(msg))
 
 
-def update(server):
-    print('Sending update', flush=True)
-    res = get_current()
-    data = {} if not res else res.json()
+def default_handler(client, server, message):
     msg = {
-        'type': 'UPDATE',
-        'timestamp': int(time.time()),
-        'data': data,
+        'type': 'ERROR',
+        'msg': 'Request type {} is not supported'.format(message.get('type'))
     }
-    print(msg, flush=True)
-    server.send_message_to_all(JSONEncoder().encode(msg))
+    server.send_message(client, JSONEncoder().encode(msg))
+
+
+def new_message(client, server, message):
+    message = json.loads(message)
+    handler = {
+        'REQUEST_CURRENCY': data_request,
+        'REQUEST_FORECAST': forecast_request,
+    }.get(message.get('type'), default_handler)(client, server, message)
+
+
+def update(server):
+    clients = server.clients.copy()
+    while clients:
+        currency = clients[0].get('currency')
+        output_currency = clients[0].get('output_currency')
+        res = get_current(currency, output_currency)
+        data = {} if not res else res.json()
+        msg = {
+            'type': 'UPDATE',
+            'timestamp': int(time.time()),
+            'data': data,
+        }
+        for client in clients:
+            if (client.get('currency'), client.get('output_currency')) == (currency, output_currency):
+                server.send_message(client, JSONEncoder().encode(msg))
+                clients.remove(client)
 
 
 def print_clients(server):
@@ -48,9 +101,8 @@ def start_server(loglevel=logging.WARNING):
     server = WebsocketServer(PORT, IP, loglevel)
     timer = Timer(5, update, stop_event, server)
     timer.start()
-    timer2 = Timer(30, print_clients, stop_event, server)
-    timer2.start()
     server.set_fn_new_client(new_client)
+    server.set_fn_message_received(new_message)
     server.run_forever()
     stop_event.clear()
     print('Gracefully stopping')
